9f4040b06bc853bfa14d12fbd8a7f2a71b2817c2
sourav-paul-JKT <sourav.paul@jktech.com>
2025-09-23 09:22:22 +0000

    login page modified.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
-import { Welcome } from './pages/Welcome'
-import { Login } from './pages/Login'
+import React, { Suspense, lazy } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const Welcome = lazy(() => import('./pages/Welcome'))
+const Login = lazy(() => import('./pages/Login'))
+const Register = lazy(() => import('./pages/Register'))
 
 function App() {
   return (
-    <>
-  <Welcome/>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div className="min-h-screen flex items-center justify-center text-white">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
